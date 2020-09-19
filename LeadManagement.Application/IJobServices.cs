0d23a3cfdda0fae1c471e15814d6c5eeffed8392
Fred Momeni <fred.momeni@3plearning.com>
2020-09-19 21:23:18 +1000

    Adding Job services

---
+++ LeadManagement.Application/IJobServices.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using LeadManagement.Application.DTOs;
+
+namespace LeadManagement.Application
+{
+    /// <summary>
+    /// The services for job
+    /// </summary>
+    public interface IJobServices
+    {
+        /// <summary>
+        /// Returns all the jobs
+        /// </summary>
+        /// <returns>
+        /// The list of jobs
+        /// </returns>
+        Task<IEnumerable<JobDto>> GetJobs();
+    }
+}
---
+++ LeadManagement.Application/JobServices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutoMapper;
+
+using LeadManagement.Application.DTOs;
+using LeadManagement.Data.DbHandlers;
+
+namespace LeadManagement.Application
+{
+    public class JobServices
+    {
+        #region Fields
+
+        private readonly IJobDbHandler jobDbHandler;
+
+        private readonly IMapper mapper;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JobServices"/> class.
+        /// </summary>
+        /// <param name="jobDbHandler">
+        /// the job database handler
+        /// </param>
+        /// <param name="mapper">
+        /// The auto mapper.
+        /// </param>
+        public JobServices(IJobDbHandler jobDbHandler, IMapper mapper)
+        {
+            this.jobDbHandler = jobDbHandler;
+            this.mapper = mapper;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public async Task<IEnumerable<JobDto>> GetJobs()
+        {
+            var jobs = await this.jobDbHandler.GetAllJobs();
+            return this.mapper.Map<IEnumerable<JobDto>>(jobs);
+        }
+
+        #endregion
+    }
+}
